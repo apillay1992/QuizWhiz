7f94ea81529c587f65886bee44865b74811ea39a
apillay023 <apillay023@student.wethinkcode.co.za>
2024-10-17 17:57:43 +0200

    set background color

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.lightBlue,
+          body: LoginPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    print('Hello');
+                  },
+                  child: const Text('Button')),
+            ]
+        )
+    );
+  }
+}
