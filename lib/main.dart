cedce1d559e85f3e77e148af464d47c7ed491946
apillay023 <apillay023@student.wethinkcode.co.za>
2024-10-18 07:03:26 +0200

    moved the scaffold from the stateless widget to the stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.lightBlue,
-          body: LoginPage(),
-        ),
-      ),
+      home: LoginPage()
     );
   }
 }
@@ -31,71 +26,74 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-            children: [
-              const SizedBox(
-                width: double.infinity,
-                height: 100,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 40, top: 40, right: 40.0, bottom: 20),
-                  child: Text('Login',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+          child: Column(
+              children: [
+                const SizedBox(
+                  width: double.infinity,
+                  height: 100,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 40, top: 40, right: 40.0, bottom: 20),
+                    child: Text('Login',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 300,
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person),
-                    labelText: 'Username',
+                const SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                      labelText: 'Username',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: double.infinity,
-                height: 30.0,
-              ),
-              const SizedBox(
-                width: 300.0,
-                height: 50.0,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                    labelText: 'Password',
+                const SizedBox(
+                  width: double.infinity,
+                  height: 30.0,
+                ),
+                const SizedBox(
+                  width: 300.0,
+                  height: 50.0,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'Password',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: double.infinity,
-                height: 50,
-              ),
-              SizedBox(
-                width: 200.0,
-                height:50.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    elevation: 0,
-                  ),
-                    onPressed: () {
-                      print('Hello');
-                    },
-                    child: const Text('login')),
-              ),
-            ]
-        )
+                const SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 200.0,
+                  height:50.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      elevation: 0,
+                    ),
+                      onPressed: () {
+                        print('Hello');
+                      },
+                      child: const Text('login')),
+                ),
+              ]
+          )
+      ),
     );
   }
 }
